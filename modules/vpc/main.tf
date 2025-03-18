f3f62e72756f454456637b4f867d11da12d909da
thrinathadevops <167942687+thrinathadevops@users.noreply.github.com>
2025-03-18 11:52:29 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}-private-${count.index}"
+  }
+}
