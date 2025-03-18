73d1a175be709fe4163785296f4c8328e2383b90
thrinathadevops <167942687+thrinathadevops@users.noreply.github.com>
2025-03-18 11:53:26 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Deployment environment name"
+  type        = string
+}
